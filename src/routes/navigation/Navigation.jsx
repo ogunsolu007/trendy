7966ddd0bdc6c8ef2d27f9370f21b49fe1670c86
electrified007 <63315246+ogunsolu007@users.noreply.github.com>
2022-12-22 03:40:55 +0100

    building navigation bar component

---
+++ src/routes/navigation/Navigation.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import { Outlet, Link } from "react-router-dom";
+import "./navigation.scss"
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrownLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link to="/shop" className="nav-link">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
